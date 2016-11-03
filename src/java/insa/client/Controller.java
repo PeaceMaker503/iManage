2b074549ed2af414858c4ae47b9b6cdf5d5d226e
zaurelzo <ahamada.abdillah06@gmail.com>
2016-11-03 16:33:33 +0100

    ajout fake controller And javascript pour tester

---
+++ src/java/insa/client/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insa.client;
+
+import insa.ws.ManageWS;
+
+/**
+ *
+ * @author zaurelzo
+ */
+public class Controller {
+    
+    
+}
