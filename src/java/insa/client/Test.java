bf870e4b9a729f2d26e4458e88c0f4762d728bb0
JordyCabannes <cabannes@etud.insa-toulouse.fr>
2016-11-05 21:27:12 +0100

    Beginning of homepage layout

--- src/java/insa/client/Test.java
+++ src/java/insa/client/Test.java
@@ -37,7 +37,7 @@ public class Test extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
