2b284781517af1c930e8c8c9990da603c2e79c7e
JordyCabannes <cabannes@etud.insa-toulouse.fr>
2016-11-05 19:47:33 +0100

    First commit with a front-end

---
+++ web/Web-Content/index.js
@@ -0,0 +1,82 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+        /*function onYouTubeIframeAPIReady() {
+                     var player;
+                     player = new YT.Player('videoPlayer', {
+                       videoId: 'M5hnrks2zow', // YouTube Video ID
+                       height: $(window).height()+'px',
+                       width: $(window).width()+'px',// Player height (in px)
+                       playerVars: {
+                         autoplay: 1,        // Auto-play the video on load
+                         controls: 0,        // Show pause/play buttons in player
+                         showinfo: 0,        // Hide the video title
+                         modestbranding: 0,  // Hide the Youtube Logo
+                         loop: 1,            // Run the video in a loop
+                         fs: 0,              // Hide the full screen button
+                         cc_load_policy: 0, // Hide closed captions
+                         iv_load_policy: 3,  // Hide the Video Annotations
+                         autohide: 1        // Hide video controls when playing
+                       },
+                       events: {
+                         onReady: function(e) {
+                           e.target.mute();
+                         }
+                       }
+                     });
+                     document.getElementById('main').style.height=$(window).height()+'px';
+                     //document.getElementById('videoPlayer').style.marginTop=-(($('#videoPlayer').height()-$(window).height())/2)+'px';
+                     //document.getElementById('videoPlayer').style.marginRight=-(($('#videoPlayer').width()-$(window).width())/2)+'px';
+                        
+                }*/
+   
+        
+        function sizeTitleApp(){
+            var largeurFenetre= $(window).width();
+            document.getElementById("titleApp").style.fontSize="60pt";
+            if(largeurFenetre<500){
+                document.getElementById("titleApp").style.fontSize="40pt";
+            }
+        }
+        
+        function clicCreationCompte(){
+            var formConnPere = document.getElementById("insideConnexion");
+            var formConn=document.getElementById("formulaireConnexion");
+//var form.
+            //while(formConn.childElementCount>0){
+                formConnPere.removeChild(formConn);
+                //form
+            //}
+
+            formConnPere.innerHTML = '<form id="formulaireCreationCompte" class="col-xs-push-1 col-xs-10 col-sm-push-2 col-sm-8 col-md-push-3 col-md-6 col-lg-push-3 col-lg-6 centrerVerticalementForm" method="post">'
+            +   '<div class="labelForm form-group">'
+            +       '<label for="motDePasse">Login</label>'
+            +       '<input type="text" id="loginCr" name="loginCr" class="form-control">'
+            +   '</div>'
+            +   '<div class="labelForm form-group">'
+            +       '<label class="labelForm" for="motDePasse">Password</label>'
+            +       '<input type="password" id="motDeaPasseCr" name="motDePasseCr" class="form-control">'
+            +   '</div>'
+            +   '<div class="labelForm form-group">'
+            +       '<label class="labelForm" for="emailAddressCr">E-mail address</label>'
+            +       '<input type="password" id="emailAddressCr" name="emailAddressCr" class="form-control">'
+            +   '</div>'               
+            +   '<div>'
+            +       '<button type="submit" class="btn btn-primary col-xs-push-2 col-xs-8 col-sm-push-3 col-sm-6 col-md-push-3 col-md-6 col-lg-push-3 col-lg-6">Sign up</button>'
+            +   '</div>'
+            +'</form>'
+            +'<div id="liensCr" class="container-fluid col-xs-12">'
+            +   '<div class="row">' 
+            +      '<div class="form-group">'
+            +         '<button id="back" class="col-xs-6 boutonsCompte">Back</button>'
+            +      '</div>'
+            +   '</div>'
+            +'</div>';            
+        }
+        
+        jQuery(document).ready(function(){
+            setInterval(sizeTitleApp, 1);
+        });
