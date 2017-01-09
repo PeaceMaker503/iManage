59e59c86032cc719f5302da9fdd5614038f1ee31
zaurelzo <ahamada.abdillah06@gmail.com>
2017-01-09 15:59:24 +0100

    fix bug with json format

--- busIntegration/my_service.py
+++ busIntegration/my_service.py
@@ -8,20 +8,23 @@ class GetInternshipDetails(Service):
     def handle(self):
         try:
             self.logger.info('Request: {}'.format(self.request.payload))
+
             with self.outgoing.soap.get('getInternshipWebService1').conn.client() as client:
-                compagny=json.loads(self.request.payload)["selectCompany"]
-                category= json.loads(self.request.payload)["selectCategory"]
-                keywords=json.loads(self.request.payload)["keywords"]
-                self.logger.info("********* " + compagny + "  "+ category +"  " +keywords)
-                listOfInternship=client.service.getInternshipByCriteria(compagny,category,keywords)
+                company=self.request.payload["selectCompany"]
+                category=self.request.payload["selectCategory"]
+                keywords=self.request.payload["keywords"]
+
+                self.logger.info("********* company : " + company +" category :"+ category + " keywords :" + keywords)
+                listOfInternship=client.service.getInternshipByCriteria(company,category,keywords)
                 self.logger.info("================ result : " + str (listOfInternship) + "  "+str(type(listOfInternship)))
-                #let's build the final response 
+                
+                # #let's build the final response 
                 finalResponse = {}
                 for ind, internship in enumerate(listOfInternship):
                     finalResponse[str(ind)]=str(internship)
-                #    self.logger.info("+++type of internship " + str(ind )+"  "+ str(type(internship)))
+                # #    self.logger.info("+++type of internship " + str(ind )+"  "+ str(type(internship)))
 
-                #return response to the caller
+                # #return response to the caller
                 self.response.payload="\""+str(finalResponse)+"\""
                 
         except Exception as e:
