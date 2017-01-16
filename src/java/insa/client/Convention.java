acf3dc56b3b05f8a4a7ed95ba4586b3efc92e028
Pablo Maldonado <prmm95@gmail.com>
2017-01-16 14:16:22 +0100

    update fields internship convention

--- src/java/insa/client/Convention.java
+++ src/java/insa/client/Convention.java
@@ -7,12 +7,21 @@ package insa.client;
 
 import insa.db.Candidature;
 import insa.db.UserAccount;
+import insa.db.UserProfile;
 import insa.ws.CandidatureWS;
 import insa.ws.UserProfileWS;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -94,62 +103,80 @@ public class Convention extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("This is a POST");
+		
+		// Parameters definition:
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = new Date();
 		String login = request.getParameter("login");
 		String cand_id = request.getParameter("cand_id");
 		long user_id = userProfileService.getUserAccountByLogin(login).getId();
-		request.setAttribute("candidatureList",candidatureService.getCandidaturesByUserID(user_id));		
-		
-		UserAccount ua = userProfileService.getUserAccountByLogin(request.getParameter("login"));
-		String userCategory = ua.getUserCategory();
+		UserAccount userAccount = userProfileService.getUserAccountByLogin(request.getParameter("login"));
+		UserProfile userProfile = userAccount.getId_profile();
+		String userCategory = userAccount.getUserCategory();
         if(userCategory.compareTo("Student")==0){
             request.setAttribute("student","true");
         }
         else{
             request.setAttribute("student","false");
         }
-			
-		// Load the pdfTemplate
-		File pdfFile = new File("/home/prmm95/Bureau/PdfFormExample.pdf");
+		
+		// Create the array with the variables:
+		Map<String, String> parametersDict = new HashMap<>();
+		parametersDict.put("nom_stagiaire",userProfile.getLastName());
+		parametersDict.put("prenom_stagiaire",userProfile.getFirstName());
+		// TODO: Add to the user profile creation?
+		parametersDict.put("sec_social_stagiaire","Sec. social");
+		// TODO: Add to the user profile creation?
+		parametersDict.put("adresse_stagiaire","Adresse");
+		parametersDict.put("tel_stagiaire",userProfile.getPhone());
+		parametersDict.put("mail_stagiaire",userProfile.getMail());
+		// TODO: This will be on the request.
+		parametersDict.put("sujet_stage","Sujet du stage");
+		// TODO: This will be on the request.
+		parametersDict.put("activites_stage","Activites du stage");
+		// TODO: This will be on the request.
+		//parametersDict.put("dates_stage","Dates du stage");
+		// TODO: This will be on the request.
+		//parametersDict.put("heures_stage","Nombre d'heures du stage");
+		// TODO: This will be on the request.
+		parametersDict.put("jours_stage","Nombre de jours du stage");
+		// TODO: This will be on the request. This corresponds
+		// To jours feries etc.
+		parametersDict.put("commentaire_stage","Commentaires du stage");
+		// TODO: This will be on the request.
+		parametersDict.put("conges_stage","#");
+		//parametersDict.put("date_convention",dateFormat.format(date));
+		
+		// Load the internship convention template:
+		File pdfFile = new File("/home/prmm95/Bureau/Internship_Convention.pdf");
 		PDDocument pdfTemplate = PDDocument.load(pdfFile);
-
 		PDDocumentCatalog docCatalog = pdfTemplate.getDocumentCatalog();
 		PDAcroForm acroForm = docCatalog.getAcroForm();
 
-		// Get field names
-		List<PDField> fieldList = acroForm.getFields();
-
-		// String the object array
-		String[] fieldArray = new String[fieldList.size()];
-		int i = 0;
-		for (PDField sField : fieldList) {
-			fieldArray[i] = sField.getFullyQualifiedName();
-			i++;
-		}
-
-		// Loop through each field in the array and do something
-		for (String f : fieldArray) {
-			PDField field = acroForm.getField(f);
-
-			System.out.println("f is: " + f);
-			if (f.contains("Family Name Text Box")) {
-				String value = login;
-				field.setValue(value);
-				field.setReadOnly(true);
-				System.out.println("printed: " + value + " to: " + f);
+		// Loop through each field in the dictionary and update the values of the template:
+		parametersDict.entrySet().forEach((Map.Entry<String, String> entry) -> {
+			PDField field = acroForm.getField(entry.getKey());
+			try {
+				field.setValue(entry.getValue());
+			} catch (IOException ex) {
+				Logger.getLogger(Convention.class.getName()).log(Level.SEVERE, null, ex);
 			}
-		}
-		
+			field.setReadOnly(true);
+			System.out.println(entry.getKey() + " / " + entry.getValue());
+		});
+			
 		// Update the convention path of the candidature on the database:
 		String conventionPath = "/home/prmm95/Bureau/Convention_" + cand_id + ".pdf";
 		Candidature candidature = candidatureService.getCandidatureById(Long.valueOf(cand_id));
 		candidature.setConventionPath(conventionPath);
-		candidatureService.updateCandidature(candidature);
-				
+		//candidatureService.updateCandidature(candidature);
+		
 		// Save the results and ensure that the document is properly closed:
 		pdfTemplate.save(conventionPath);
 		pdfTemplate.close();
 		
+		// Update the candidatures list and redirect to the candidatures view:		
+		request.setAttribute("candidatureList",candidatureService.getCandidaturesByUserID(user_id));
 		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/Candidatures.jsp").forward(request, response);
 			
 	}
